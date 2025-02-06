8d390465217262fa813b6777de9d529c98e1774d
Fran Perez <franjpeca@gmail.com>
2025-02-06 19:28:46 +0100

    Inicio del funcionamiento de Kedro y pipeline de tratamiento de los daos

---
+++ src/data_processing/nodes/data_cleaning.py
@@ -0,0 +1,4 @@
+def clean_data(dataset):
+    print(f"Limpieza de datos: {dataset}")
+    cleaned_dataset = f"{dataset}_limpiado"
+    return cleaned_dataset
---
+++ src/data_processing/nodes/data_generation.py
@@ -0,0 +1,4 @@
+def generate_data():
+    print("\n\n----------------------------------------------------------------------")
+    print("\nInicio del primer nodo. Generando datos...")
+    return "datos_generados"
